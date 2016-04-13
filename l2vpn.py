095de4f9a321f77fbc24a324fa40ffa97fbe9b36
gavmckee80 <gavmckee80@gmail.com>
2016-04-13 10:34:34 -0400

    Delete l2vpn.py

--- l2vpn.py
+++
@@ -1,7 +0,0 @@
-"""
-Pythonifier for Route Kompella L2VPN
-"""
-from jnpr.junos.factory import loadyaml
-from os.path import splitext
-_YAML_ = splitext(__file__)[0] + '.yml'
-globals().update(loadyaml(_YAML_))
