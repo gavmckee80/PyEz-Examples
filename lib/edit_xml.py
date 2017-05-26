ff5f3fd2833c901bbf8d9bb0679534ad5a81a806
gmckee <gavmckee80@gmail.com>
2017-05-25 20:57:30 -0400

    Just messing around with some git

--- lib/edit_xml.py
+++ lib/edit_xml.py
@@ -1,4 +1,5 @@
 # Writing some code to allow us to modify an xml reponse to unit test the code
+#just some ideas
 
 # to remove an entire route table from an XML file the following code looks good
 for x in rt.xml.xpath('route-table'):
